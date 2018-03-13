4e2912591fe1f83dbea0a7a38c4c5c469b9d204f
Ryan C <ryanjchen2@gmail.com>
2018-03-13 11:37:13 -0400

    struggling with animations

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def helloWorld():
-    return "Hello world!"
+    return render_template('index.html')
