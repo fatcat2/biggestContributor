4af07bfb3d265e3393adfa262c4e01459704c304
Ryan C <ryanjchen2@gmail.com>
2018-03-13 20:47:42 -0400

    starbucks wifi didn't work

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from flask import Flask
-import os
+import os, praw, operator
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return render_template('index.html')
+    reddit = praw.Reddit(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'], password=os.environ["password"], user_agent="biggestContributor", username="papertow3ls")
+    dict = {}
+    for submission in reddit.subreddit('BlackPeopleTwitter').hot(limit=1000000000):
+        author = submission.author.name
+        if(not dict.has_key(author)):
+            dict[author] = 1
+        else:
+            count = dict[author]
+            count += 1
+            dict[author] = count
+    
+    sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
+    sorted_dict.reverse()
+
+    stop_count = 0
+
+    return render_template('index.html', results=sorted_dict[0:10])
+
+@app.route('/search', methods=['GET', 'POST')
