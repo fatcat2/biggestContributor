6f4ee7bfe4de905de69b152c8898bac0635571f0
patrick <kelly237@purdue.edu>
2018-03-16 17:21:50 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # biggestContributor
-Who are the biggest contributers on your reddit sub? Check it out here.
+Who are the biggest contributers on your reddit sub? Check it out [here](
+https://agile-eyrie-77894.herokuapp.com/).
+
+### Dependencies:
+- PRAW (Python)
+- Flask (Python)
+- [Materialize.css](http://materializecss.com/) (HTML)
+
+### Description
+This is a flask app that uses the PRAW library in order to get information about subreddits. Currently a WIP.
+
+### Issues
+Currently doesn't work on mobile.
+
+### Laundry List
+- Have the menu show up on mobile
+- Graphs! Maybe D3.js
--- main.py
+++ main.py
@@ -1,48 +1,62 @@
-from flask import Flask
+#!/usr/bin/env python2
+
+# "python" defaults to a 2.7 shell on Linux, check it's the same for MacOSX
+
+from flask import Flask, redirect, render_template, request # one-liner import for flask
 import os, praw, operator
-from flask import render_template
-from flask import request
 
 
 app = Flask(__name__)
+
+siteName = "http://127.0.0.1:5000" # change this to an environment var, Someday^TM
+
 reddit = praw.Reddit(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'], password=os.environ["password"], user_agent="biggestContributor", username="papertow3ls")
 
-@app.route('/')
-def helloWorld():
+def internalHelloWorld():
+
     dict = {}
     score_dict = {}
     for submission in reddit.subreddit('BlackPeopleTwitter').hot(limit=1000000000):
         if(submission.author == None):
             continue
         author = submission.author.name
-        if(not dict.has_key(author)):
+        if(not author in dict):
             dict[author] = 1
         else:
             count = dict[author]
             count += 1
             dict[author] = count
-        if(not score_dict.has_key(author)):
+        if(not author in score_dict):
             score_dict[author] = submission.score
         else:
             count = score_dict[author]
             count += submission.score
             score_dict[author] = count
-    
+
     sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
     sorted_dict.reverse()
-    final_list = [];
+    final_list = []
     for list in sorted_dict:
         list = list + (score_dict[list[0]],)
         #print list
         final_list.append(list)
     return render_template('index.html', sub='BlackPeopleTwitter', results=final_list[0:10])
 
+
+@app.route('/')
+def helloWorld(): # must immediately return something, can not have any other code or it throws an error in flask on Linux
+
+    return redirect("{}/search".format(siteName))
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def hello():
     return render_template('search.html')
 
+
 @app.route('/results', methods=['GET', 'POST'])
 def showResults():
+
     dict = {}
     score_dict = {}
     sub_name = request.args.get('sub', '')
@@ -66,7 +80,7 @@ def showResults():
             score_dict[author] = count
     sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
     sorted_dict.reverse()
-    final_list = [];
+    final_list = []
     for list in sorted_dict:
         list = list + (score_dict[list[0]],)
         final_list.append(list)
