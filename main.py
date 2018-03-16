304b33bab9517511a0a6f8920eaf1fe83d91ebfc
Ryan C <ryanjchen2@gmail.com>
2018-03-16 17:02:47 -0400

    Revert "changed has_key to 'in'" yikes pat This reverts commit 59bbe2cb80cfb1a4ca3da1ad44ae8ca1d0001fff.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # "python" defaults to a 2.7 shell on Linux, check it's the same for MacOSX
 
@@ -12,41 +12,37 @@ siteName = "http://127.0.0.1:5000" # change this to an environment var, Someday^
 
 reddit = praw.Reddit(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'], password=os.environ["password"], user_agent="biggestContributor", username="papertow3ls")
 
-def internalHelloWorld():
-
-    dict = {}
-    score_dict = {}
-    for submission in reddit.subreddit('BlackPeopleTwitter').hot(limit=1000000000):
-        if(submission.author == None):
-            continue
-        author = submission.author.name
-        if(not author in dict):
-            dict[author] = 1
-        else:
-            count = dict[author]
-            count += 1
-            dict[author] = count
-        if(not author in score_dict):
-            score_dict[author] = submission.score
-        else:
-            count = score_dict[author]
-            count += submission.score
-            score_dict[author] = count
-
-    sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
-    sorted_dict.reverse()
-    final_list = []
-    for list in sorted_dict:
-        list = list + (score_dict[list[0]],)
-        #print list
-        final_list.append(list)
-    return render_template('index.html', sub='BlackPeopleTwitter', results=final_list[0:10])
-
-
 @app.route('/')
 def helloWorld(): # must immediately return something, can not have any other code or it throws an error in flask on Linux
 
     return redirect("{}/search".format(siteName))
+    # dict = {}
+    # score_dict = {}
+    # for submission in reddit.subreddit('BlackPeopleTwitter').hot(limit=1000000000):
+    #     if(submission.author == None):
+    #         continue
+    #     author = submission.author.name
+    #     if(not dict.has_key(author)):
+    #         dict[author] = 1
+    #     else:
+    #         count = dict[author]
+    #         count += 1
+    #         dict[author] = count
+    #     if(not score_dict.has_key(author)):
+    #         score_dict[author] = submission.score
+    #     else:
+    #         count = score_dict[author]
+    #         count += submission.score
+    #         score_dict[author] = count
+    #
+    # sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
+    # sorted_dict.reverse()
+    # final_list = [];
+    # for list in sorted_dict:
+    #     list = list + (score_dict[list[0]],)
+    #     #print list
+    #     final_list.append(list)
+    # return render_template('index.html', sub='BlackPeopleTwitter', results=final_list[0:10])
 
 
 @app.route('/search', methods=['GET', 'POST'])
