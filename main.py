b8122a03fcb931cbdb977750efe3b95ca5f5fb84
Ryan Chen <ryanjchen2@gmail.com>
2020-05-10 17:55:44 -0700

    Refactored the get_subreddit_results message

--- main.py
+++ main.py
@@ -13,33 +13,9 @@ reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'], client_secret=os.environ
 
 @app.route('/')
 def index():
-    dict = {}
-    score_dict = {}
-    for submission in reddit.subreddit('Purdue').hot(limit=1000000000):
-        if(submission.author == None):
-            continue
-        author = submission.author.name
-        if(author not in dict):
-            dict[author] = 1
-        else:
-            count = dict[author]
-            count += 1
-            dict[author] = count
-        if(not author in score_dict):
-            score_dict[author] = submission.score
-        else:
-            count = score_dict[author]
-            count += submission.score
-            score_dict[author] = count
+    subreddit_results = get_subreddit_results("Purdue", 100)
 
-    sorted_dict = sorted(dict.items(), key=operator.itemgetter(1))
-    sorted_dict.reverse()
-    final_list = []
-    for list in sorted_dict:
-        list = list + (score_dict[list[0]],)
-        final_list.append(list)
-    return render_template('index.html', sub='Purdue', results=final_list)
-
+    return render_template("index.html", sub='Purdue', results=subreddit_results)
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -47,7 +23,7 @@ def search_route():
     return render_template('search.html')
 
 
-def get_subreddit_results(subreddit_name: str, limit_num: int, reddit: praw.Reddit) -> List[Dict[Any, Any]]:
+def get_subreddit_results(subreddit_name: str, limit_num: int) -> List[Dict[Any, Any]]:
     """Gets highest contributing redditors to a specific subreddit.
 
     Args:
@@ -55,6 +31,8 @@ def get_subreddit_results(subreddit_name: str, limit_num: int, reddit: praw.Redd
             limit_num (int): the number of posts to look at
             reddit (praw.Reddit): a praw instance to use
     """
+    ret_dict = {}
+    score_dict = {}
     sub = reddit.subreddit(subreddit_name)
 
     for submission in sub.new(limit=int(limit_num)):
@@ -69,7 +47,8 @@ def get_subreddit_results(subreddit_name: str, limit_num: int, reddit: praw.Redd
             score_dict[author] = submission.score
         else:
             score_dict[author] += submission.score
-    sorted_dict = sorted(dict.items(), key=operator.itemgetter(1)).reverse()
+    sorted_dict = sorted(ret_dict.items(), key=operator.itemgetter(1))
+    sorted_dict.reverse()
     final_list = []
     for list in sorted_dict:
         list = list + (score_dict[list[0]],)
@@ -83,7 +62,7 @@ def results_route():
     sub_name = request.args.get('sub', '')
     limit_num = request.args.get('num', '')
 
-    subreddit_results = get_subreddit_results(sub_name, int(limit_num), reddit)
+    subreddit_results = get_subreddit_results(sub_name, int(limit_num))
 
     return render_template('index.html', sub=sub_name, results=subreddit_results)
 
