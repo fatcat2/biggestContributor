ecaea01b029739d2941c89b2bf4c3938a41d69aa
patrick <kelly237@purdue.edu>
2020-05-19 15:36:32 -0400

    added get_descendant_metadata and traverse_comment_chain for traversing the descendant comment tree.

--- utils/reddit_search.py
+++ utils/reddit_search.py
@@ -6,6 +6,8 @@ import praw
 from .types import Redditor, SubNotFoundException
 
 
+g_flattened_comments: praw.models.reddit.comment.Comment = []
+
 def get_subreddit_results(
     subreddit_name: str, limit_num: int, reddit: praw.Reddit
 ) -> List[Dict[Any, Any]]:
@@ -23,6 +25,7 @@ def get_subreddit_results(
         for submission in sub.new(limit=int(limit_num)):
             if submission.author == None:
                 continue
+            submission_descendants = get_descendant_metadata()
             author = submission.author.name
             if author not in redditors:
                 redditors[author] = Redditor(
@@ -36,8 +39,46 @@ def get_subreddit_results(
     return_list = [r.serialize() for r in redditors.values()]
     sorted_return_list = sorted(return_list, key=lambda r: r["post_score"])
     sorted_return_list.reverse()
-
     for counter in range(0, len(sorted_return_list)):
         sorted_return_list[counter]["rank"] = counter + 1
 
     return sorted_return_list
+
+
+
+def get_descendant_metadata(submission: praw.models.reddit.submission.Submission) -> Dict[Redditor, int]:
+    """Get author and score information from all descendants of submission.
+        
+        Args:
+            submission (praw.models.reddit.submission.Submission): the submission who's descendants we're interested in
+        
+        Returns:
+            A Dict[Redditor, int] of all descendants
+    """
+
+    
+    for child in submission.comments:
+        traverse_comment_chain(child)
+
+    # add data from the flattened tree
+    children_author_and_score: Dict[Redditor, int] = {}
+    for comment in g_flattened_comments:
+        children_author_and_score[comment.author] = comment.score
+    
+    return children_author_and_score
+
+
+def traverse_comment_chain(comment: praw.models.reddit.comment.Comment) -> None:
+    """Traverses all descendants of comment, differs from get_descendant_metadata in that it recurses down the tree.
+
+        Args:
+            comment (praw.models.reddit.comment.Comment): The root of the tree to traverse
+    """
+    g_flattened_comments.append(comment)
+
+    if len(comment.replies) == 0:
+        return None
+    
+    for child in comment.replies:
+        traverse_comment_chain(child)
+    
